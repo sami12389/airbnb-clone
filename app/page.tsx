63f4fcf5c184390e855aaccb01134357de47b6bf
Samson Lemma <69362509+sami12389@users.noreply.github.com>
2023-10-04 21:37:15 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,38 +1,56 @@
-import {ClientOnly, Container, EmptyState, ListingCard} from "./components/index"
-import getListings from "./actions/getListings";
-import getCurrentUser from "./actions/getCurrentUser";
-import getReservations from "@/app/actions/getReservations"
+import Container from "@/app/components/Container";
+import ListingCard from "@/app/components/listings/ListingCard";
+import EmptyState from "@/app/components/EmptyState";
 
+import getListings, { 
+  IListingsParams
+} from "@/app/actions/getListings";
+import getCurrentUser from "@/app/actions/getCurrentUser";
+import ClientOnly from "./components/ClientOnly";
 
+interface HomeProps {
+  searchParams: IListingsParams
+};
 
-export default async function Home() {
-  const listings = await getListings()
-  const currentUser = await getCurrentUser()
+const Home = async ({ searchParams }: HomeProps) => {
+  const listings = await getListings(searchParams);
+  const currentUser = await getCurrentUser();
 
-  if(listings?.length === 0){
-    return(
+  if (listings.length === 0) {
+    return (
       <ClientOnly>
-        <EmptyState showReset/>
+        <EmptyState showReset />
       </ClientOnly>
-    )
+    );
   }
+
   return (
-  <ClientOnly>
-    <Container>
-      <div className = "pt-24 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
-       {listings?.map((listing: any)=>{
-        return(
-          <div>
+    <ClientOnly>
+      <Container>
+        <div 
+          className="
+            pt-24
+            grid 
+            grid-cols-1 
+            sm:grid-cols-2 
+            md:grid-cols-3 
+            lg:grid-cols-4
+            xl:grid-cols-5
+            2xl:grid-cols-6
+            gap-8
+          "
+        >
+          {listings.map((listing: any) => (
             <ListingCard
-            currentUser = {currentUser}
-            key = {listing.id}
-            data = {listing}
+              currentUser={currentUser}
+              key={listing.id}
+              data={listing}
             />
-          </div>
-        )
-       })}
-      </div>
-    </Container>
-  </ClientOnly>
+          ))}
+        </div>
+      </Container>
+    </ClientOnly>
   )
 }
+
+export default Home;
